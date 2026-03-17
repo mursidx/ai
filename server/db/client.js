ff1472254436944408a1f5c3ef330ca121a5f4dc
Mursid - X <iammursid@gmail.com>
2026-03-17 11:07:37 +0530

    Delete server directory

--- server/db/client.js
+++
@@ -1,6 +0,0 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('server/db/app.db');
-db.pragma('journal_mode = WAL');
-
-export default db;
--- server/services/balanceService.js
+++
@@ -1,6 +0,0 @@
-import db from '../db/client.js';
-
-export function partyOutstanding(businessId, partyId) {
-  const inv = db.prepare("SELECT COALESCE(SUM(grand_total),0) t, COALESCE(SUM(amount_paid),0) p FROM invoices WHERE business_id=? AND party_id=? AND type='invoice'").get(businessId, partyId);
-  return Number(inv.t) - Number(inv.p);
-}
